b190268cc9a7421228f598e478c05eec0205b731
雷俊俊 <junjun.lei@hand-china.com>
2020-11-30 17:36:46 +0800

    [ADD]:项目初始化

---
+++ src/main/java/com/junjunlei/EurekaApplication7001.java
@@ -0,0 +1,17 @@
+package com.junjunlei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author junjun.lei@hand-china.com
+ * @create 2020-02-29 15:12
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication7001.class,args);
+    }
+}
